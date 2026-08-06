3e890353224c788dec7284c00ace84afc557fd02
Sushil Patil <1514@prospectprecise.com>
2026-08-07 00:29:20 +0530

    remove vite

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
 export default defineConfig({
+  server: {
+    allowedHosts: ['usual-catlike-unfitting.ngrok-free.dev'],
+  },
   plugins: [
     react(),
     babel({
-      presets: ['react-compiler'],
+      presets: [reactCompilerPreset()],
     }),
   ],
-
-  build: {
-    outDir: "dist",
-  },
 })
